19e8d4cdd1ef6b6e781984b502ca422e53565177
vkajtazov <kajtazov.viktor@gmail.com>
2015-06-05 13:21:03 +0200

    fixed bug in prescription service

--- src/main/java/mk/ukim/finki/ezdravstvo/service/impl/PrescriptionServiceImpl.java
+++ src/main/java/mk/ukim/finki/ezdravstvo/service/impl/PrescriptionServiceImpl.java
@@ -5,7 +5,9 @@ import mk.ukim.finki.ezdravstvo.repository.PrescriptionRepository;
 import mk.ukim.finki.ezdravstvo.service.PrescriptionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PrescriptionServiceImpl extends
 		BaseEntityCrudServiceImpl<Prescription, PrescriptionRepository>
 		implements PrescriptionService {
