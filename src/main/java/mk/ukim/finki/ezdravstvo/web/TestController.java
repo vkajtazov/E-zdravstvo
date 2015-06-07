3277324de08a411155437071ba09d1330998a09b
vkajtazov <kajtazov.viktor@gmail.com>
2015-06-08 00:19:19 +0200

    test routes for utf-8 support

--- src/main/java/mk/ukim/finki/ezdravstvo/web/TestController.java
+++ src/main/java/mk/ukim/finki/ezdravstvo/web/TestController.java
@@ -1,13 +1,14 @@
 package mk.ukim.finki.ezdravstvo.web;
 
-import java.sql.Date;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
-import mk.ukim.finki.ezdravstvo.model.AppointmentBooking;
 import mk.ukim.finki.ezdravstvo.model.Doctor;
 import mk.ukim.finki.ezdravstvo.model.EmailMessage;
 import mk.ukim.finki.ezdravstvo.model.Patient;
 import mk.ukim.finki.ezdravstvo.model.TimeSlots;
-import mk.ukim.finki.ezdravstvo.repository.AppointmentBookingRepository;
+import mk.ukim.finki.ezdravstvo.model.User;
 import mk.ukim.finki.ezdravstvo.repository.TimeSlotsRepository;
 import mk.ukim.finki.ezdravstvo.service.AppointmentBookingService;
 import mk.ukim.finki.ezdravstvo.service.DoctorService;
@@ -15,6 +16,7 @@ import mk.ukim.finki.ezdravstvo.service.PatientService;
 import mk.ukim.finki.ezdravstvo.service.mail.EmailNotificationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,24 +30,42 @@ public class TestController {
 
 	@Autowired
 	private EmailNotificationService emailNotificationService;
-	
+
 	@Autowired
 	private TimeSlotsRepository timeSlotsRepository;
 
 	@Autowired
 	private AppointmentBookingService bookingService;
-	
+
 	@Autowired
 	private PatientService patientService;
 
 	@RequestMapping(value = "/addDoctor", method = RequestMethod.GET, produces = "application/json")
 	public Doctor createDoctor() {
 		Doctor tmp = new Doctor();
-		tmp.setFistName("Name");
-		tmp.setUsername("Doctor1");
+		tmp.setFistName("Бла бла доктор");
+		tmp.setUsername("blabla");
 		tmp = (Doctor) tmp;
 		tmp.setPassword("qwe");
 		return doctorService.saveAndFlush(tmp);
+	}
+
+	@RequestMapping(value = "/testValueGet", method = RequestMethod.GET, produces = "application/json")
+	public User testValueGet(@RequestBody @Valid User entity,
+			HttpServletRequest request, HttpServletResponse response) {
+		return entity;
+	}
+	
+	@RequestMapping(value = "/testValuePost", method = RequestMethod.POST, produces = "application/json")
+	public User testValuePost(@RequestBody @Valid User entity,
+			HttpServletRequest request, HttpServletResponse response) {
+		return entity;
+	}
+	
+	@RequestMapping(value = "/testValueValid", method = RequestMethod.POST, produces = "application/json")
+	public User testValueValid(@RequestBody  User entity,
+			HttpServletRequest request, HttpServletResponse response) {
+		return entity;
 	}
 
 	@RequestMapping("/send_email_test")
@@ -57,15 +77,14 @@ public class TestController {
 		emailNotificationService.sendEmail(message);
 	}
 
-
 	@RequestMapping("/book")
 	public void testBooking() {
 		java.util.Date date = new java.util.Date();
-		
+
 		Doctor d = doctorService.findOne((long) 6);
 		Patient p = patientService.findOne((long) 1);
 		TimeSlots timeSlot = timeSlotsRepository.findOne((long) 1);
-		
-		//bookingService.bookAppointment(date, timeSlot, d);
+
+		// bookingService.bookAppointment(date, timeSlot, d);
 	}
 }
